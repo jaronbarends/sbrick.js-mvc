ee853398a309b79ef2fe3e2cf997f79795af02c0
jaronbarends <jaron.barends@valtech.nl>
2017-06-15 08:39:25 +0200

    added servo control

--- js/controller.js
+++ js/controller.js
@@ -262,7 +262,7 @@
 		// Per the specs, this has to be done IN RESPONSE TO A USER ACTION
 		connectBtn.addEventListener('click', connectHandler);
 
-		log('v10');
+		log('v0.11');
 	};
 
 	// kick of the script when all dom content has loaded
