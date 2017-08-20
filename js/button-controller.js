eccaf42d6e444c5e7b81b45b3b55239fd8e85e98
jaronbarends <jaron.barends@valtech.nl>
2017-08-20 20:12:26 +0200

    check for isArray in stop-method was called before second forEach loop. Moved it to before first forEach

--- js/button-controller.js
+++ js/button-controller.js
@@ -312,6 +312,11 @@
 		sensorSwitch = document.getElementById('toggle-sensor');
 		sensorSwitch.addEventListener('click', toggleSensor);
 
+		document.getElementById('stop-3').addEventListener('click', (e) => {
+			e.preventDefault();
+			mySBrick.stop(3);
+		});
+
 		// set listeners for sbrick events
 		document.body.addEventListener('portchange.sbrick', portchangeHandler);
 		document.body.addEventListener('sensorchange.sbrick', sensorchangeHandler);
